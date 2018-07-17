538cecd8084fe48b6963436d196bcb2a5cbedd79
Chris Webster <cgw57@hotmail.com>
2018-07-17 15:40:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function nowServing(katzDeliLine) {
   }
 }
 
-function currentLine(kazsDeliLine) {
+function currentLine(kazDeliLine) {
     if (katzDeliLine.length === 0) {
     return "The line is currently empty.";
   }
