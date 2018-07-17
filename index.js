b5ac91b91840f4655daa21e7cfc8b56ee0ee697a
Chris Webster <cgw57@hotmail.com>
2018-07-17 15:23:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ function takeANumber(katzDeliLine, name) {
   return ("Welcome, "+ name + ". You are number " +(katzDeliLine.length)+ " in line.");
 }
 
-function nowServing() {
-  
+function nowServing(katsDeliLine) {
+  if (katsDeliLine === 0) {
+    return "There is nobody waiting to be served!.";
+  }
 }
