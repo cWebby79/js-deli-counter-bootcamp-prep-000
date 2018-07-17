37b48a3518e8497de1ca463a25f1b21988df02f2
Chris Webster <cgw57@hotmail.com>
2018-07-17 15:16:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ var katzDeli = [];
 
 function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name);
-  return (katzDeliLine.length);
+  return ("Welcome, "+ name + ". You are number " +(katzDeliLine.length)+ " in line.");
 }
