7a17ce6eee1e8c5b9625e5471a1aefbeeeaac28f
Chris Webster <cgw57@hotmail.com>
2018-07-17 15:39:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,16 +5,16 @@ function takeANumber(katzDeliLine, name) {
   return ("Welcome, "+ name + ". You are number " +(katzDeliLine.length)+ " in line.");
 }
 
-function nowServing(katsDeliLine) {
-  if (katsDeliLine.length === 0) {
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length === 0) {
     return "There is nobody waiting to be served!";
   } else {
     return ("Currently serving " + katsDeliLine.shift() + ".");
   }
 }
 
-function currentLine(katsDeliLine) {
-  if (katzDeliLine.length === 0) {
+function currentLine(kazsDeliLine) {
+    if (katzDeliLine.length === 0) {
     return "The line is currently empty.";
   }
 }
