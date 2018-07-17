6f6e6de6809ffe7b56895e0fe866883833cb9db9
Chris Webster <cgw57@hotmail.com>
2018-07-17 15:47:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,6 +17,8 @@ function currentLine(katzDeliLine) {
     if (katzDeliLine.length === 0) {
     return "The line is currently empty.";
   } else {
-    return "The line is currently: " + katzDeliLine + ". "+ katzDeliLine[]
+    for (let i = 0; i < katzDeliLine.length; i++) {
+      return ("The line is currently: " + katzDeliLine + katzDeliLine[i]);
+    }
   }
 }
